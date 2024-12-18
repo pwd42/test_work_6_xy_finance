3ae337761f3ab1fca4e435e78386b671cfe0f2b0
pwd42 <102031732+pwd42@users.noreply.github.com>
2024-12-18 16:45:43 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,18 @@
+RPC_URLS = {
+    "Arbitrum": "https://endpoints.omniatech.io/v1/arbitrum/one/public",
+    "Optimism": "https://op-pokt.nodies.app",
+    "Base": "https://1rpc.io/base"
+    # "Base": "https://base-pokt.nodies.app"
+}
+
+EXPLORERS_URL = {
+    "Arbitrum": "https://arbiscan.io/",
+    "Optimism": "https://optimistic.etherscan.io/",
+    "Base": "https://basescan.org/"
+}
+
+CHAIN_ID_BY_NAME = {
+    'Arbitrum': 42161,
+    'Optimism': 10,
+    'Base': 8453
+}
